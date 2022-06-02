9790b37e13651f9668e246d31d10d123d1edff5e
Igor Sulim <su24svp@gmail.com>
2022-06-03 00:03:11 +0300

    theThirdCommit

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,29 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
-gulp.task('server', function() {
+gulp.task('server', function() { // Здесть "server" просто название.
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "src" // Откуда грузим файлы для отображения в браузере
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload); //обновление страницы при изменении .html
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+    return gulp.src("src/sass/**/*.+(scss|sass)") // здесь указываем из какой папки берем файлы и с какими расширениями
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) //настройка того, в каком стиле компиллируется файл .css
+        .pipe(rename({suffix: '.min', prefix: ''})) // переименование скомпиллированного файла .css
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("src/css")) // Куда отправляем скомпилированный файл.
+        .pipe(browserSync.stream()); // Обновление страницы после автокомпиляции и всех действий.
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles')); // отслеживание изменения файлов, запуск задачи "styles"
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles')); // запуск задач Gulp параллельно
